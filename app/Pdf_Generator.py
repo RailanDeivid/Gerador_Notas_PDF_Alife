9d5d46d9d86a809e26e5aafba19791a8e5c08114
Railan Deivid <contato.railandeivid@gmail.com>
2025-04-02 10:49:11 -0300

    update

--- app/Pdf_Generator.py
+++ app/Pdf_Generator.py
@@ -23,11 +23,7 @@ def salvar_numero_nota(numero):
     with open(ARQUIVO_NUMERO_NOTA, "w") as f:
         f.write(str(numero))
 
-# Inicializar o número da nota no session_state
-if "numero_nota" not in st.session_state:
-    st.session_state.numero_nota = carregar_numero_nota()
 
-st.write(f"Número da Nota Atual: {st.session_state.numero_nota}")
 
 
 
@@ -217,6 +213,11 @@ with open(logo_path, "rb") as img_file:
 st.set_page_config(page_title="Gerador de Notas de Débito em PDF",
                    page_icon=logo_bytes, layout="wide", initial_sidebar_state="expanded")
 
+# Inicializar o número da nota no session_state
+if "numero_nota" not in st.session_state:
+    st.session_state.numero_nota = carregar_numero_nota()
+
+st.write(f"Número da Nota Atual: {st.session_state.numero_nota}")
 
 # Setar título
 st.markdown("""
